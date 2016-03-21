a217f2abe24a77cefc0cc78eb73378cb1296d002
liuqiuchen <2016968116@qq.com>
2016-03-21 20:57:20 +0800

    1

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
+
+    var config = {
+        app:'app',
+        dist:'dist'
+    };
+
+    grunt.initConfig({
+        config:config,
+
+        copy: {
+            dist: {
+                src:'<%= config.app %>/index.html',
+                dest:'<%= config.dist %>/index.html'
+            }
+        }
+    });
+};
